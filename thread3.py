4e15d3de2e33722cc1bd4cf11618710846d80532
Christian Ducros <96232175+christianDUCROS@users.noreply.github.com>
2023-06-15 06:39:29 +0200

    thread3.py

--- thread3.py
+++ thread3.py
@@ -17,3 +17,26 @@ while True :
     print('debut boucle du core0',utime.ticks_ms())
     print("J’utilise le coeur 0")
     utime.sleep(5)
+
+'''
+#fonction time out dans le thread
+import utime as time
+import _thread
+
+def core1_thread():
+    start = time.ticks_ms()
+    while True :
+        print("J’utilise le coeur 1")
+        time.sleep(2)
+        delta = time.ticks_diff(time.ticks_ms(), start)
+        if delta  > 10000 : 
+            raise SystemExit
+
+_thread.start_new_thread(core1_thread, ())
+
+# main
+while True :
+    print('debut boucle du core0',time.ticks_ms())
+    print("J’utilise le coeur 0")
+    time.sleep(5)
+'''
